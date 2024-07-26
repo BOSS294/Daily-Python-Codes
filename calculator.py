6e2161910ff33c6d3853fa702a80b2cf43618a91
Mayank Chawdhari <72921622+BOSS294@users.noreply.github.com>
2024-07-26 23:21:58 +0530

    PANDA

--- calculator.py
+++ calculator.py
@@ -2,8 +2,7 @@ def calculator():
     try:
         expression = input("Enter a mathematical expression: ")
         result = eval(expression)
-        result_as_integer = int(result)
-        print(f"The result is: {result_as_integer}")
+        print(f"The result is: {int(result)}")
     except Exception as e:
         print(f"Error: {e}")
 
